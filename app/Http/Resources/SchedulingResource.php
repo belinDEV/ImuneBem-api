6ef4fee19c6c532f4a993da522cf475f8103f926
MaceiraDev <leo.maceira.santos@hotmail.com>
2024-06-26 20:30:29 -0300

    status_id adiciona a resource de agendamento

--- app/Http/Resources/SchedulingResource.php
+++ app/Http/Resources/SchedulingResource.php
@@ -10,10 +10,12 @@ class SchedulingResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'patient_id' => $this->patient_id,
             'patient_name' => $this->patient->name,
+            'professional_id' => optional($this->professional)->id,
             'professional_name' => optional($this->professional)->name,
             'vaccine_name' => optional($this->vaccines)->name,
-            'status' => $this->status_id,
+            'status_id' => $this->status_id,
             'status_name' => $this->status->description,
             'date' => $this->date,
             'description' => $this->description,
