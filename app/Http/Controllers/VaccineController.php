acfa38df30ced83d6c23743e4844f0dc4c7ad43a
Caio Henrique <caiogrechic50@gmail.com>
2024-06-27 13:18:47 -0300

    Corrigido rotas de vacinas

--- app/Http/Controllers/VaccineController.php
+++ app/Http/Controllers/VaccineController.php
@@ -16,23 +16,10 @@ class VaccineController extends Controller
     {
         $this->responceService = $responceService;
     }
-    
-    public function index(Request $request)
+
+    public function index()
     {
-        // Pega o id do usuario salvo no token
-        $userId = $request->attributes->get('user_id');
-        // Recupera o usuário
-        $user = User::find($userId);
-        // Se for admin retorna todos os pacientes
-        if ($user->type_user == 0) {
-            $vaccines = Vaccine::all();
-            // Se for cuidador retorna todos os pacientes que estão associados a ele
-        } else if ($user->type_user == 1) {
-            $vaccines = Vaccine::where('user_id', $userId)->get();
-            // Se for usuário retorna o próprio cadastro no sistema
-        } else {
-            $vaccines = Vaccine::where('id', $userId)->get();
-        }
+        $vaccines = Vaccine::all();
         return VaccineResource::collection($vaccines);
     }
 
