a0ffd62c4f8c966a725ff77ef72bb58b014afe6a
MaceiraDev <leo.maceira.santos@hotmail.com>
2024-06-26 21:33:39 -0300

    infos controller finalizada

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Scheduling;
 use App\Models\Vaccine;
 use Illuminate\Http\Request;
 
@@ -9,9 +10,22 @@ class InfoController extends Controller
 {
     public function index()
     {
-        $totalVaccines = Vaccine::sum('id');
+        $totalVaccines = Vaccine::count('id');
+
+        $schedolingPendents = Scheduling::where('status_id', 1);
+        $pendents = $schedolingPendents->count('id');
+
+        $schedolingAceppts = Scheduling::where('status_id', 2);
+        $aceppts = $schedolingAceppts->count('id');
+
+        $schedolingFinal = Scheduling::where('status_id', 3);
+        $final = $schedolingFinal->count('id');
+
         return response()->json([
             'total_vaccines' => $totalVaccines,
+            'schedoling_pendents' => $pendents,
+            'schedoling_aceppts' => $aceppts,
+            'schedoling_final' => $final,
         ]);
     }
 }
